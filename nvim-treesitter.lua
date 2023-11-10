4f7e24bbc0495a5e0cf9b224997451423d0d6c5a
tay.io <128968711+berlintay@users.noreply.github.com>
2023-11-10 07:01:27 -0400

    nvim config

---
+++ nvim-treesitter.lua
@@ -0,0 +1,38 @@
+local config = function()
+  require("nvim-treesitter.configs").setup({
+    indent = {
+      enable = true,
+    },
+    autotag = {
+      enable = true,
+    },
+    ensure_installed = {
+      "markdown",
+      "json",
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "markdown",
+      "bash",
+      "lua",
+      "dockerfile",
+      "solidity",
+      "gitignore",
+      "python",
+      "go",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = true,
+    },
+  })
+end
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false, 
+  config = config
+}
+
